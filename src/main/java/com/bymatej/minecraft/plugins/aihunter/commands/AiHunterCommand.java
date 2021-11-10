0209af28cef6cbe2ef22eee7a64a7a05ea2d8355
Matej Jelić <matej.jelic@digacon-software.com>
2021-11-10 17:10:13 +0100

    Refactor the command

--- src/main/java/com/bymatej/minecraft/plugins/aihunter/commands/AiHunterCommand.java
+++ src/main/java/com/bymatej/minecraft/plugins/aihunter/commands/AiHunterCommand.java
@@ -1,19 +1,13 @@
 package com.bymatej.minecraft.plugins.aihunter.commands;
 
-import com.bymatej.minecraft.plugins.aihunter.events.HunterToggleEvent;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 import static com.bymatej.minecraft.plugins.aihunter.utils.CommonUtils.log;
-import static com.bymatej.minecraft.plugins.aihunter.utils.Constants.COMMAND_AIHUNTER_ARGUMENT_OFF;
-import static com.bymatej.minecraft.plugins.aihunter.utils.Constants.COMMAND_AIHUNTER_ARGUMENT_ON;
-import static com.bymatej.minecraft.plugins.aihunter.utils.HunterStatus.OFF;
-import static com.bymatej.minecraft.plugins.aihunter.utils.HunterStatus.ON;
-import static java.util.logging.Level.INFO;
+import static java.lang.Integer.parseInt;
 import static java.util.logging.Level.WARNING;
-import static org.apache.commons.lang.StringUtils.isBlank;
-import static org.bukkit.Bukkit.getPluginManager;
 
 public class AiHunterCommand implements CommandExecutor {
 
@@ -36,64 +30,90 @@ public class AiHunterCommand implements CommandExecutor {
     private void executeCommand(CommandSender sender, String[] args) throws CommandException {
         log("Command is executing");
 
-        validateCommand(sender, args);
-        Player aiHunter = getAiHunterPlayer(sender, args[0]);
-        if (COMMAND_AIHUNTER_ARGUMENT_ON.equalsIgnoreCase(args[1])) {
-            turnAiHunterOn(aiHunter);
-        } else if (COMMAND_AIHUNTER_ARGUMENT_OFF.equalsIgnoreCase(args[1])) {
-            turnAiHunterOff(aiHunter);
+        if (sender instanceof Player) {
+            if (sender.hasPermission("terminatornpc.spawnterminator")) { //todo: rename permission
+                validateCommand(sender, args);
+
+                if (args.length == 1) {
+                    turnAiHunterOn(args[0], (Player) sender);
+                }
+                if (args.length == 2) {
+                    turnAiHunterOn(args[0], parseInt(args[1]), (Player) sender);
+                }
+            }
         } else {
-            String message = "Unrecognized parameter " + args[1];
+            String message = "You cannot execute this command. You're not a Player, or you don't have the permission.";
             sender.sendMessage(message);
             log(WARNING, message);
             throw new CommandException(message);
         }
 
-    }
-
-    private Player getAiHunterPlayer(CommandSender sender, String playerName) {
-        Player player = Bukkit.getPlayer(playerName);
-        if (player == null) {
-            String message = "Player was not found (got null).";
-            sender.sendMessage(message);
-            log(WARNING, message);
-            throw new CommandException(message);
-        }
-
-        return player;
     }
 
     private void validateCommand(CommandSender sender, String[] args) throws CommandException {
+        if (args.length == 1) {
+            validateDesiredAiHunterName(sender, args[0]);
+        }
+
+        if (args.length == 2) {
+            validateDesiredAiHunterName(sender, args[0]);
+            validateDesiredHunterAmount(sender, args[1]);
+        }
+
         if (args.length > 2) {
-            String message = "There should not be more than two arguments for this command.";
+            String message = "More than 2 parameters given. Unrecognized request";
             sender.sendMessage(message);
             log(WARNING, message);
             throw new CommandException(message);
-        } else if (args.length < 2) {
-            String message = "There should be at least two arguments for this command.";
-            sender.sendMessage(message);
-            log(WARNING, message);
-            throw new CommandException(message);
-        } else if (isBlank(args[0]) || isBlank(args[1])) {
-            String message = "Both first and second arguments must not be blank or null.";
-            sender.sendMessage(message);
-            log(WARNING, message);
-            throw new CommandException(message);
-        } else {
-            log(INFO, "Both arguments present are correct: " + args[0] + " and " + args[1]);
         }
     }
 
-    private void turnAiHunterOn(Player aiHunter) {
-        HunterToggleEvent hunterToggleEvent = new HunterToggleEvent(aiHunter, ON);
-        getPluginManager().callEvent(hunterToggleEvent);
-        log("Hunter turned on");
+    private void validateDesiredAiHunterName(CommandSender sender, String aiHunterName) {
+        // Name must not be null/blank
+        if (StringUtils.isBlank(aiHunterName)) {
+            String message = "Hunter name must not be blank!";
+            sender.sendMessage(message);
+            log(WARNING, message);
+            throw new CommandException(message);
+        }
+
+        // Player with that name must not exist (although, if you name your hunter "ABC" and later on the player with that name joins - that's OK)
+        Player player = Bukkit.getPlayer(aiHunterName);
+        if (player != null) {
+            String message = "A real player with that game already exists on the server. Don't try to trick people!";
+            sender.sendMessage(message);
+            log(WARNING, message);
+            throw new CommandException(message);
+        }
     }
 
-    private void turnAiHunterOff(Player aiHunter) {
-        HunterToggleEvent hunterToggleEvent = new HunterToggleEvent(aiHunter, OFF);
-        getPluginManager().callEvent(hunterToggleEvent);
-        log("Hunter turned off");
+    private void validateDesiredHunterAmount(CommandSender sender, String desiredNumberOfHunters) {
+        int numberOfHunters = 0;
+        try {
+            numberOfHunters = parseInt(desiredNumberOfHunters);
+        } catch (NumberFormatException e) {
+            String message = String.format("\"%s\" is not a number!", desiredNumberOfHunters);
+            sender.sendMessage(message);
+            log(WARNING, message);
+            throw new CommandException(message);
+        }
+
+        if (numberOfHunters < 1 || numberOfHunters > 200) {
+            String message = "Number of hunters is 0 or it is greater than 200. Not enough, or enough to kill a server. Chang the number!";
+            sender.sendMessage(message);
+            log(WARNING, message);
+            throw new CommandException(message);
+        }
+    }
+
+    private void turnAiHunterOn(String aiHunterName, Player player) {
+        turnAiHunterOn(aiHunterName, 1, player);
+    }
+
+    private void turnAiHunterOn(String aiHunterName, int amountOfHunters, Player player) {
+//        HunterToggleEvent hunterToggleEvent = new HunterToggleEvent(aiHunterName, ON); //todo: refactor HunterToggleEvent constructor/fields
+//        getPluginManager().callEvent(hunterToggleEvent);
+//        log("Hunter turned on");
     }
 
 }
--- src/main/java/com/bymatej/minecraft/plugins/aihunter/utils/Constants.java
+++ src/main/java/com/bymatej/minecraft/plugins/aihunter/utils/Constants.java
@@ -2,7 +2,6 @@ package com.bymatej.minecraft.plugins.aihunter.utils;
 
 public class Constants {
 
-    public static final String COMMAND_AIHUNTER_ARGUMENT_ON = "on";
-    public static final String COMMAND_AIHUNTER_ARGUMENT_OFF = "off";
+    // todo: delete if empty
 
 }
